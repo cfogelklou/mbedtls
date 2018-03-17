cce9882a17676a8901fc4120972b1f2ae4ddbd28
Chris Fogelklou <chris.fogelklou@gmail.com>
2018-03-17 08:43:50 +0100

    edcsa signatures verify against reference signatures from the pkix document.

--- library/ecdsa.c
+++ library/ecdsa.c
@@ -65,6 +65,10 @@ cleanup:
     return( ret );
 }
 
+#if defined(MBEDTLS_ECP_DP_ED25519_ENABLED)
+#include "crypto_sign_ed25519.h"
+#endif
+
 /*
  * Compute ECDSA signature of a hashed message (SEC1 4.1.3)
  * Obviously, compared to SEC1 4.1.3, we skip step 4 (hash message)
@@ -73,90 +77,107 @@ int mbedtls_ecdsa_sign( mbedtls_ecp_group *grp, mbedtls_mpi *r, mbedtls_mpi *s,
                 const mbedtls_mpi *d, const unsigned char *buf, size_t blen,
                 int (*f_rng)(void *, unsigned char *, size_t), void *p_rng )
 {
+#if defined(MBEDTLS_ECP_DP_ED25519_ENABLED)
+  if (grp->id == MBEDTLS_ECP_DP_ED25519) {
+    int ret;
+    uint8_t sk[crypto_sign_ed25519_SECRETKEYBYTES];
+    uint8_t sig[crypto_sign_ed25519_BYTES];
+    ret = mbedtls_mpi_write_binary(d, sk, crypto_sign_ed25519_SECRETKEYBYTES);
+    if (0 == ret) {
+      unsigned long long siglen = crypto_sign_ed25519_BYTES;
+      ret = crypto_sign_ed25519_detached(sig, &siglen, buf, blen, sk);
+      mbedtls_mpi_read_binary(r, sig, siglen);
+      mbedtls_mpi_lset(s, 0);
+    }
+    return ret;
+  }
+  else {
+#endif
     int ret, key_tries, sign_tries, blind_tries;
     mbedtls_ecp_point R;
     mbedtls_mpi k, e, t;
 
     /* Fail cleanly on curves such as Curve25519 that can't be used for ECDSA */
-    if( grp->N.p == NULL )
-        return( MBEDTLS_ERR_ECP_BAD_INPUT_DATA );
+    if (grp->N.p == NULL)
+      return(MBEDTLS_ERR_ECP_BAD_INPUT_DATA);
 
-    mbedtls_ecp_point_init( &R );
-    mbedtls_mpi_init( &k ); mbedtls_mpi_init( &e ); mbedtls_mpi_init( &t );
+    mbedtls_ecp_point_init(&R);
+    mbedtls_mpi_init(&k); mbedtls_mpi_init(&e); mbedtls_mpi_init(&t);
 
     sign_tries = 0;
     do
     {
-        /*
-         * Steps 1-3: generate a suitable ephemeral keypair
-         * and set r = xR mod n
-         */
-        key_tries = 0;
-        do
+      /*
+       * Steps 1-3: generate a suitable ephemeral keypair
+       * and set r = xR mod n
+       */
+      key_tries = 0;
+      do
+      {
+        MBEDTLS_MPI_CHK(mbedtls_ecp_gen_keypair(grp, &k, &R, f_rng, p_rng));
+        MBEDTLS_MPI_CHK(mbedtls_mpi_mod_mpi(r, &R.X, &grp->N));
+
+        if (key_tries++ > 10)
         {
-            MBEDTLS_MPI_CHK( mbedtls_ecp_gen_keypair( grp, &k, &R, f_rng, p_rng ) );
-            MBEDTLS_MPI_CHK( mbedtls_mpi_mod_mpi( r, &R.X, &grp->N ) );
-
-            if( key_tries++ > 10 )
-            {
-                ret = MBEDTLS_ERR_ECP_RANDOM_FAILED;
-                goto cleanup;
-            }
+          ret = MBEDTLS_ERR_ECP_RANDOM_FAILED;
+          goto cleanup;
         }
-        while( mbedtls_mpi_cmp_int( r, 0 ) == 0 );
+      } while (mbedtls_mpi_cmp_int(r, 0) == 0);
 
-        /*
-         * Step 5: derive MPI from hashed message
-         */
-        MBEDTLS_MPI_CHK( derive_mpi( grp, &e, buf, blen ) );
+      /*
+       * Step 5: derive MPI from hashed message
+       */
+      MBEDTLS_MPI_CHK(derive_mpi(grp, &e, buf, blen));
 
-        /*
-         * Generate a random value to blind inv_mod in next step,
-         * avoiding a potential timing leak.
-         */
-        blind_tries = 0;
-        do
-        {
-            size_t n_size = ( grp->nbits + 7 ) / 8;
-            MBEDTLS_MPI_CHK( mbedtls_mpi_fill_random( &t, n_size, f_rng, p_rng ) );
-            MBEDTLS_MPI_CHK( mbedtls_mpi_shift_r( &t, 8 * n_size - grp->nbits ) );
+      /*
+       * Generate a random value to blind inv_mod in next step,
+       * avoiding a potential timing leak.
+       */
+      blind_tries = 0;
+      do
+      {
+        size_t n_size = (grp->nbits + 7) / 8;
+        MBEDTLS_MPI_CHK(mbedtls_mpi_fill_random(&t, n_size, f_rng, p_rng));
+        MBEDTLS_MPI_CHK(mbedtls_mpi_shift_r(&t, 8 * n_size - grp->nbits));
 
-            /* See mbedtls_ecp_gen_keypair() */
-            if( ++blind_tries > 30 )
-                return( MBEDTLS_ERR_ECP_RANDOM_FAILED );
-        }
-        while( mbedtls_mpi_cmp_int( &t, 1 ) < 0 ||
-               mbedtls_mpi_cmp_mpi( &t, &grp->N ) >= 0 );
+        /* See mbedtls_ecp_gen_keypair() */
+        if (++blind_tries > 30)
+          return(MBEDTLS_ERR_ECP_RANDOM_FAILED);
+      } while (mbedtls_mpi_cmp_int(&t, 1) < 0 ||
+        mbedtls_mpi_cmp_mpi(&t, &grp->N) >= 0);
 
-        /*
-         * Step 6: compute s = (e + r * d) / k = t (e + rd) / (kt) mod n
-         */
-        MBEDTLS_MPI_CHK( mbedtls_mpi_mul_mpi( s, r, d ) );
-        MBEDTLS_MPI_CHK( mbedtls_mpi_add_mpi( &e, &e, s ) );
-        MBEDTLS_MPI_CHK( mbedtls_mpi_mul_mpi( &e, &e, &t ) );
-        MBEDTLS_MPI_CHK( mbedtls_mpi_mul_mpi( &k, &k, &t ) );
-        MBEDTLS_MPI_CHK( mbedtls_mpi_inv_mod( s, &k, &grp->N ) );
-        MBEDTLS_MPI_CHK( mbedtls_mpi_mul_mpi( s, s, &e ) );
-        MBEDTLS_MPI_CHK( mbedtls_mpi_mod_mpi( s, s, &grp->N ) );
+      /*
+       * Step 6: compute s = (e + r * d) / k = t (e + rd) / (kt) mod n
+       */
+      MBEDTLS_MPI_CHK(mbedtls_mpi_mul_mpi(s, r, d));
+      MBEDTLS_MPI_CHK(mbedtls_mpi_add_mpi(&e, &e, s));
+      MBEDTLS_MPI_CHK(mbedtls_mpi_mul_mpi(&e, &e, &t));
+      MBEDTLS_MPI_CHK(mbedtls_mpi_mul_mpi(&k, &k, &t));
+      MBEDTLS_MPI_CHK(mbedtls_mpi_inv_mod(s, &k, &grp->N));
+      MBEDTLS_MPI_CHK(mbedtls_mpi_mul_mpi(s, s, &e));
+      MBEDTLS_MPI_CHK(mbedtls_mpi_mod_mpi(s, s, &grp->N));
 
-        if( sign_tries++ > 10 )
-        {
-            ret = MBEDTLS_ERR_ECP_RANDOM_FAILED;
-            goto cleanup;
-        }
-    }
-    while( mbedtls_mpi_cmp_int( s, 0 ) == 0 );
+      if (sign_tries++ > 10)
+      {
+        ret = MBEDTLS_ERR_ECP_RANDOM_FAILED;
+        goto cleanup;
+      }
+    } while (mbedtls_mpi_cmp_int(s, 0) == 0);
 
-cleanup:
-    mbedtls_ecp_point_free( &R );
-    mbedtls_mpi_free( &k ); mbedtls_mpi_free( &e ); mbedtls_mpi_free( &t );
+
+  cleanup:
+    mbedtls_ecp_point_free(&R);
+    mbedtls_mpi_free(&k); mbedtls_mpi_free(&e); mbedtls_mpi_free(&t);
 
     return( ret );
-}
 
 #if defined(MBEDTLS_ECP_DP_ED25519_ENABLED)
-#include "crypto_sign_ed25519.h"
+  }
 #endif
+
+}
+
+
 
 #if defined(MBEDTLS_ECDSA_DETERMINISTIC)
 /*
